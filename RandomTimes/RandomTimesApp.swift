3612a07e16517f7cbab3b0d60213fb7223c95015
Matthew <matthedm@uci.edu>
2022-09-19 13:10:44 -0700

    Initial Commit

---
+++ RandomTimes/RandomTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomTimesApp.swift
+//  RandomTimes
+//
+//  Created by m on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
